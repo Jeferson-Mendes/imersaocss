9647c5709db27899f3515d8dfdb44fbee883c1f0
Jeferson-Mendes <56761644+Jeferson-Mendes@users.noreply.github.com>
2022-06-21 13:29:20 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ arrow.addEventListener('click', ()=> {
 	if ( arrow.classList.contains('arrowHidden')) {
 		menuLeft.style.left = '0px'
 		arrow.classList.remove('arrowHidden')
-		scrollContainer.style.left = '150px';
+		scrollContainer.style.paddingLeft = '180px';
 	}
 	else {
 		menuLeft.style.position = 'absolute'
 		menuLeft.style.left = '-280px'
 		arrow.classList.add('arrowHidden')	
-		scrollContainer.style.left = '0';
+		scrollContainer.style.paddingLeft = '0';
 	}
 	
 })
@@ -42,17 +42,17 @@ const contact = document.querySelector('.contacts');
 
 let scrollTopValue = 0
 
-handleEventListener(circle01, 	0  , [circle02, circle03, circle04, circle05] , [circle01], [skills, jobs, blog, contact] , [beginning] ) // circle01
-handleEventListener(circle02, -100 , [circle01, circle03, circle04, circle05] , [circle02], [beginning , jobs, blog, contact] , [skills] ) // circle02
-handleEventListener(circle03, -200 , [circle01, circle02, circle04, circle05] , [circle03], [beginning , skills, blog, contact] , [jobs] ) // circle03
-handleEventListener(circle04, -300 , [circle01, circle02, circle03, circle05] , [circle04], [beginning , skills, jobs, contact] , [blog] ) // circle04
-handleEventListener(circle05, -400 , [circle01, circle02, circle03, circle04] , [circle05], [beginning , skills, jobs, blog] , [contact] ) // circle05
+handleEventListener(circle01, 	'presentation'  , [circle02, circle03, circle04, circle05] , [circle01], [skills, jobs, blog, contact] , [beginning] ) // circle01
+handleEventListener(circle02, 'skills' , [circle01, circle03, circle04, circle05] , [circle02], [beginning , jobs, blog, contact] , [skills] ) // circle02
+handleEventListener(circle03, 'projects' , [circle01, circle02, circle04, circle05] , [circle03], [beginning , skills, blog, contact] , [jobs] ) // circle03
+handleEventListener(circle04, 'blog' , [circle01, circle02, circle03, circle05] , [circle04], [beginning , skills, jobs, contact] , [blog] ) // circle04
+handleEventListener(circle05, 'contacts' , [circle01, circle02, circle03, circle04] , [circle05], [beginning , skills, jobs, blog] , [contact] ) // circle05
 
-handleEventListener(beginning, 0  ,  [circle02, circle03, circle04, circle05] , [circle01], [skills, jobs, blog, contact] , [beginning] ) // circle01
-handleEventListener(skills,  -100 ,  [circle01, circle03, circle04, circle05] , [circle02], [beginning , jobs, blog, contact] , [skills] ) // circle02
-handleEventListener(jobs, 	 -200 ,  [circle01, circle02, circle04, circle05] , [circle03], [beginning , skills, blog, contact] , [jobs] ) // circle03
-handleEventListener(blog, 	 -300 ,  [circle01, circle02, circle03, circle05] , [circle04], [beginning , skills, jobs, contact] , [blog] ) // circle05
-handleEventListener(contact, -400 ,  [circle01, circle02, circle03, circle04] , [circle05], [beginning , skills, jobs, blog] , [contact] ) // circle06
+handleEventListener(beginning, 'presentation'  ,  [circle02, circle03, circle04, circle05] , [circle01], [skills, jobs, blog, contact] , [beginning] ) // circle01
+handleEventListener(skills,  'skills' ,  [circle01, circle03, circle04, circle05] , [circle02], [beginning , jobs, blog, contact] , [skills] ) // circle02
+handleEventListener(jobs, 	 'projects' ,  [circle01, circle02, circle04, circle05] , [circle03], [beginning , skills, blog, contact] , [jobs] ) // circle03
+handleEventListener(blog, 	 'blog' ,  [circle01, circle02, circle03, circle05] , [circle04], [beginning , skills, jobs, contact] , [blog] ) // circle05
+handleEventListener(contact, 'contacts' ,  [circle01, circle02, circle03, circle04] , [circle05], [beginning , skills, jobs, blog] , [contact] ) // circle06
 
 
 
@@ -65,8 +65,8 @@ window.addEventListener('wheel', (event)=> {
 		if(!loading) {
 			loading = true;
 
-			scrollContainer.style.top = conditionToScrollTopToBottom()	
-
+			// scrollContainer.style.top = conditionToScrollTopToBottom()	
+			window.location.href = `#${conditionToScrollTopToBottom()}`
 			setTimeout(()=>{
         		loading = false;
           	},1000);		
@@ -76,21 +76,21 @@ window.addEventListener('wheel', (event)=> {
 		if(!loading) {
 			loading = true;
 
-			scrollContainer.style.top = conditionToScrollBottomToTop()	
-
+			// scrollContainer.style.top = conditionToScrollBottomToTop()	
+			window.location.href = `#${conditionToScrollTopToBottom()}`
 			setTimeout(()=>{
         		loading = false;
-          	},2000);		
+          	},1000);		
 		}
 	}
 
 })
 
 
-function handleEventListener( element , scrollContainerValue , circlesToRemoveClass , circleToAddClass, itemsToRemoveClass, itemsToAddClass ) {
+function handleEventListener( element , sessionId , circlesToRemoveClass , circleToAddClass, itemsToRemoveClass, itemsToAddClass ) {
 	element.addEventListener('click', ()=> {
-		scrollContainer.style.top = `${scrollContainerValue}%`
-
+		// scrollContainer.style.top = `${scrollContainerValue}%`
+		window.location.href = `#${sessionId}`
 		removeCircleClass(circlesToRemoveClass, itemsToRemoveClass)
 		addCircleClass(circleToAddClass, itemsToAddClass)
 
@@ -118,31 +118,31 @@ function conditionToScrollTopToBottom() {
 			scrollTopValue = -100;
 			removeCircleClass([circle01, circle03, circle04, circle05], [beginning , jobs, blog, contact])
 			addCircleClass([circle02], [skills])
-			return '-100%'
+			return 'skills'
 			break;
 		case -100:			
 			scrollTopValue = -200;
 			removeCircleClass([circle01, circle02, circle04, circle05], [beginning , skills, blog, contact])
 			addCircleClass([circle03], [jobs])
-			return '-200%'
+			return 'projects'
 			break;
 		case -200:
 			scrollTopValue = -300;
 			removeCircleClass([circle01, circle02, circle03, circle05], [beginning ,skills, jobs, contact])
 			addCircleClass([circle04], [blog])
-			return '-300%'
+			return 'blog'
 			break;
 		case -300:
 			scrollTopValue = -400;
 			removeCircleClass([circle01, circle02, circle03, circle04], [beginning ,skills, jobs, blog])
 			addCircleClass([circle05], [contact])
-			return '-400%'
+			return 'contacts'
 			break;
 		case -400:
 			scrollTopValue = 0;
 			removeCircleClass([circle02, circle03, circle04, circle05], [skills, jobs, blog, contact])
 			addCircleClass([circle01], [beginning ])
-			return '0%'
+			return 'presentation'
 			break;
 	}
 }
@@ -151,38 +151,38 @@ function conditionToScrollBottomToTop() {
 	switch (scrollTopValue) {
 		
 		case 0:			
-			scrollTopValue = -400;
+			scrollTopValue = 400;
 			removeCircleClass([circle01, circle02, circle03, circle04], [beginning ,skills, jobs, blog])
 			addCircleClass([circle05], [contact])
-			return '-400%'
+			return 'contacts'
 			break;
 
-		case -400:
-			scrollTopValue = -300;
+		case 400:
+			scrollTopValue = 300;
 			removeCircleClass([circle01, circle02, circle03, circle05], [beginning ,skills, jobs, contact])
 			addCircleClass([circle04], [blog])
-			return '-300%'
+			return 'blog'
 			break;
 
-		case -300:
-			scrollTopValue = -200;
+		case 300:
+			scrollTopValue = 200;
 			removeCircleClass([circle01, circle02, circle04, circle05], [beginning , skills, blog, contact])
 			addCircleClass([circle03], [jobs])
-			return '-200%'
+			return 'projects'
 			break;
 
-		case -200:
-			scrollTopValue = -100;
+		case 200:
+			scrollTopValue = 100;
 			removeCircleClass([circle01, circle03, circle04, circle05], [beginning , jobs, blog, contact])
 			addCircleClass([circle02], [skills])
-			return '-100%'
+			return 'skills'
 			break;
 
-		case -100:
+		case 100:
 			scrollTopValue = 0;
 			removeCircleClass([circle02, circle03, circle04, circle05], [skills, jobs, blog, contact])
 			addCircleClass([circle01], [beginning ])
-			return '0'
+			return 'presentation'
 			break;
 	}	
 }
